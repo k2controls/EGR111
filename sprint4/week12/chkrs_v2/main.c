36add67188b92b1ff368a827ef7ffe5c0ebfa575
k2controls <keith@k2controls.net>
2025-04-06 19:27:14 -0400

    w12

--- sprint4/week12/chkrs_v2/main.c
+++ sprint4/week12/chkrs_v2/main.c
@@ -1,75 +1,95 @@
 // Checkers in C
-// Version 3.0
+// Version 2.0
 //  - includes jump and testing
-//  - forward only moves and kings not implemented
+//  - kings moves implemented
+//  - kinging not implemented
 // Keith E. Kelly
-// 11/4/2023
+// 04/06/2024
 
 #include "checkers.h"
 
 #if !TESTING
 
+int* get_selected_checker_from_user(int* brd_ptr, int player, bool has_jump);
+int get_selected_move_vector_from_user(int* brd_ptr, int*chkr_ptr, bool has_jump);
+
 int main(void)
 {
     int board[ROW_COUNT][COL_COUNT]; 
     int* brd_ptr = &board[0][0];
-    bool play_again = true;
-    int player, chkr_number, move_vector;
+    bool has_jump, has_second_jump;
     int* chkr_ptr;
-    bool brd_has_jump, has_jump, has_move;
-    bool jumped, moved;
+    int player, move_vector;
+    bool play_again = true;
 
     while (play_again)
     {
         make_board(brd_ptr);
         fill_board(brd_ptr);
         player = BLACK;    
-        draw_board(brd_ptr);
-        while (true)
+        while (!check_game_over(brd_ptr))
         {
-            brd_has_jump = false;
-            has_jump = false;
-            has_move = false;
-            jumped = false;
-            moved = false;
-                        
-            // get mover
-            while (!(has_jump || has_move))
-            {
-                chkr_number = get_user_checker_number(brd_ptr, player);
-                chkr_ptr = locate_cell(brd_ptr, chkr_number);
-                brd_has_jump = board_has_jump(brd_ptr);
-                if (brd_has_jump)
-                    has_jump = cell_has_jump(brd_ptr, chkr_ptr);
-                else
-                    has_move = cell_has_move(brd_ptr, chkr_ptr);
-                
-                if (brd_has_jump && !has_jump)
-                    printf("There is a jump on the board. You must move that checker.\n");
-                else if (!(has_jump || has_move))
-                    printf("No move possible for that checker. Please select another.\n");
-            }
-            //make move
-            while (!(jumped || moved))
-            {
-                move_vector = get_user_move_vector();
-                if (has_jump)
-                    jumped = jump_checker(brd_ptr, chkr_ptr, move_vector);
-                else
-                    moved = move_checker(brd_ptr, chkr_ptr, move_vector);
-                if (!(jumped || moved))
-                    printf("Invalid move. Please try again.\n");
-            }
             draw_board(brd_ptr);
-            // check status
-            if (check_game_over(brd_ptr))
-                break;
-            player = next_player(player);    
+            has_jump = board_has_jump(brd_ptr, player);
+            chkr_ptr = get_selected_checker_from_user(brd_ptr, player, has_jump);
+            move_vector = get_selected_move_vector_from_user(brd_ptr, chkr_ptr, has_jump);
+            if (has_jump)
+                jump_checker(brd_ptr, chkr_ptr, move_vector);
+            else
+                move_checker(brd_ptr, chkr_ptr, move_vector);
+            
+            has_second_jump = board_has_jump(brd_ptr, player);
+            if (has_jump && has_second_jump)
+                printf("Additional jump available. Player continues!");
+            else
+                player = get_next_player(player);
         }
         show_winner(player);
         play_again = check_play_again();
     }
     return 0;
+}
+
+int* get_selected_checker_from_user(int* brd_ptr, int player, bool has_jump)
+{
+    bool valid_chkr = false;
+    int chkr_num;
+    int* chkr_ptr;
+    while (!valid_chkr)
+    {
+        chkr_num = get_user_checker_number(brd_ptr, player);
+        chkr_ptr = locate_player_checker_cell(brd_ptr, player, chkr_num);
+        if (has_jump && cell_has_jump(brd_ptr, chkr_ptr))
+            valid_chkr = true;
+        else if (has_jump)                
+            printf("There is a jump on the board. You must move that checker.\n");
+        else if (cell_has_move(brd_ptr, chkr_ptr))
+            valid_chkr = true;
+        else
+            printf("No move possible for that checker. Please select another.\n");
+    }
+    return chkr_ptr;
+}
+
+int get_selected_move_vector_from_user(int* brd_ptr, int*chkr_ptr, bool has_jump)
+{
+    bool valid_move = false;
+    int move_vector;
+
+    while(!valid_move)
+    {
+        move_vector = get_user_move_vector(chkr_ptr);
+        if (has_jump && is_valid_jump(brd_ptr, chkr_ptr, move_vector))
+            valid_move = true;
+        else if (has_jump)
+            printf("That's not valid. A jump is required. Please try again.\n");
+        else if (is_valid_move(brd_ptr, chkr_ptr, move_vector))
+            valid_move = true;
+        else
+            printf("That's not a valid move. Please try again.\n");
+
+    }
+    return move_vector;
 }
 
 #endif
